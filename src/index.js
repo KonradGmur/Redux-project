776e458f0d82d4138367bc098544d9706d4a55ac
Konrad <57108310+KonradGmur@users.noreply.github.com>
2020-07-23 16:19:12 +0200

    Add todo.reducer.js and update App.js, index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
 import App from "./App";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import rootReducer from "./reducers";
+
+const store = createStore(rootReducer);
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   rootElement
 );
