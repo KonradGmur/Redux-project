7c6bd3681fbc58e750c33a1a330d38a38eb8c446
Konrad <57108310+KonradGmur@users.noreply.github.com>
2020-07-24 12:26:10 +0200

    Creation and extension of Todo.js functions

--- src/containers/TodoList.js
+++ src/containers/TodoList.js
@@ -1,8 +1,16 @@
 import React from "react";
 import { connect } from "react-redux";
+import Todo from "../components/Todo";
 
 const TodoList = ({ todos }) => {
-  return <pre>{JSON.stringify(todos, null, 2)}</pre>;
+  const toggleTodo = () => {};
+  return (
+    <ul>
+      {todos.map(todo => (
+        <Todo key={todo.id} {...todo} onClick={() => toggleTodo(todo.id)} />
+      ))}
+    </ul>
+  );
 };
 
 const mapStateToProps = state => ({
