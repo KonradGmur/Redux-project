cfc26e259f16e2dcaeb5f58f6fe11a481d10be2b
Konrad <57108310+KonradGmur@users.noreply.github.com>
2020-07-24 13:53:49 +0200

    Update index.js. Use of middleware in Redux

--- src/index.js
+++ src/index.js
@@ -2,10 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./reducers";
 
 const store = createStore(rootReducer);
+
+const logger = store => next => action => {
+  console.group(action.type);
+  console.info("dispatching", action);
+  let result = next(action);
+  console.log("next state", store.getState());
+  console.groupEnd();
+  return result;
+};
+
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
